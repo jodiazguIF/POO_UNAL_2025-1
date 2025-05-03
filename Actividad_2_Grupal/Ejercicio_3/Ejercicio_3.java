516b3378a62b4e06b3baff2a1014e862c3ce5b95
jtorreslo <jtorreslo@unal.edu.co>
2025-05-02 22:54:54 -0500

    Update Ejercicio_3.java

--- Actividad_2_Grupal/Ejercicio_3/Ejercicio_3.java
+++ Actividad_2_Grupal/Ejercicio_3/Ejercicio_3.java
@@ -1,5 +1,18 @@
 package Actividad_2_Grupal.Ejercicio_3;
 
 public class Ejercicio_3 {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Automovil auto1 = new Automovil("onda",2020,"kawasaki",4,8,200,160,TipoCo.GASOLINA,TipoC.NEGRO,TipoA.CIUDAD);
+        auto1.imprimir();
+        auto1.setVelocidadActual(100);
+        System.out.println("Velocidad actual = " + auto1.VelocidadActual);
+        auto1.Acelerar(20);
+        System.out.println("Velocidad actual = " + auto1.VelocidadActual);
+        auto1.Desacelerar(50);
+        System.out.println("Velocidad actual = " + auto1.VelocidadActual);
+        auto1.Frenar();
+        System.out.println("Velocidad actual = " + auto1.VelocidadActual);
+        auto1.Desacelerar(20);
+    }
 }
