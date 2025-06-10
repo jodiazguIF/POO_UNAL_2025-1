5c1de33d0bd91773653536d823cdd6d56e18b2a1
jtorreslo <jtorreslo@unal.edu.co>
2025-06-09 22:36:23 -0500

    Create Principal.java

---
+++ Actividad_4_Grupal_Ejercicio2/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.figuras;
+
+/**
+ *
+ * @author USUARIO
+ */
+ public class Principal {
+ /**
+ * Método main que sirve de punto de entrada al programa
+ */
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal= new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true); 
+        miVentanaPrincipal.setResizable(false);
+ }
+ }
+
