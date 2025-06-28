c90bce86beac5432e8ca7cdd506c725a0a6424a8
jtorreslo <jtorreslo@unal.edu.co>
2025-06-27 23:40:21 -0500

    Create ArchivosXD.java

---
+++ Actividad_5_Grupal/Ejercicio_5/ArchivosXD.java
@@ -0,0 +1,13 @@
+package com.mycompany.archivosxd;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class ArchivosXD {
+
+    public static void main(String[] args) {
+        Principal form = new Principal();
+        form.setVisible(true);
+    }
+}
